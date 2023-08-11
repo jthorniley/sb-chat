48d7e3d186606b50554d88eb39fa6a24a4e2ac3e
James Thorniley <james.thorniley@mixcloud.com>
2023-08-11 09:59:37 +0100

    add message send listener

--- app/views.py
+++ app/views.py
@@ -1,4 +1,12 @@
+from typing import Any, Dict
 from django.views.generic.base import TemplateView
-
+import re
+from django.http import Http404
 class RoomView(TemplateView):
     template_name = "app/room.html"
+
+    def get_context_data(self, room_name: str) -> Dict[str, Any]:
+        if not re.match("^[A-Za-z0-9_\- ]+$", room_name):
+            # unsafe / unprintable room name, no
+            raise Http404
+        return {"room_name": room_name}
