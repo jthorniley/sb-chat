89eb04a3a7eec1e35adc7e3a2e1d23360402cf5a
James Thorniley <james.thorniley@mixcloud.com>
2023-08-11 12:01:23 +0100

    Keep any additional context variables

--- app/views.py
+++ app/views.py
@@ -2,11 +2,16 @@ from typing import Any, Dict
 from django.views.generic.base import TemplateView
 import re
 from django.http import Http404
+
+
 class RoomView(TemplateView):
     template_name = "app/room.html"
 
-    def get_context_data(self, room_name: str) -> Dict[str, Any]:
+    def get_context_data(self, room_name: str, **kwargs) -> Dict[str, Any]:
         if not re.match("^[A-Za-z0-9_\- ]+$", room_name):
             # unsafe / unprintable room name, no
             raise Http404
-        return {"room_name": room_name}
+        return {
+            **kwargs,
+            "room_name": room_name,
+        }
